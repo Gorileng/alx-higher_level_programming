06d675e85cbcc250d45e5756cf60c2432735ad12
Gorileng <keneilwe963@gmail.com>
2023-07-11 17:45:37 -0700

    updated

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,17 +1,11 @@
-#!/usr/bin/python3
-"""Module 8-class_to_json
-Return a dictionary description with a simple data structure
-(list, dictionary, string, integer and boolean)
-for the JSON serialization of the object
+i#!/usr/bin/python3
+"""
+    8-class_to_json: class_to_json()
 """
 
 
 def class_to_json(obj):
-    """Create the dict description of the object 
-
-    Args:
-        - obj: object to be serialized
-    Returns: the dictionary descrption of the object 
     """
-
+        return dictionary description with the simple data structure.
+    """
     return obj.__dict__
