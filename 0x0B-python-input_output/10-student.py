fde0a50a1adf4459520fd67482424f9de1b1533c
Gorileng <keneilwe963@gmail.com>
2023-07-11 18:06:13 -0700

    updated

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,38 +1,37 @@
 #!/usr/bin/python3
-"""Module 10-student
-Create the student file
+"""
+    10-student: class Student
 """
 
 
 class Student:
-    """Class which defines the student
-    Public attributes:
-        - first_name
-        - last_name
-        - age
-    Public method to_json()
     """
-
+        The class students that defines the student by:
+        Attributes:
+            first_name (str): name of the student.
+            last_name (str): name of the student.
+            age (int): age of the student.
+        Methods:
+            __init__ - initialize a Student instance.
+            to_json - retrieve the dictionary representation of the Student instance.
+    """
     def __init__(self, first_name, last_name, age):
-        """Initialize a student instance"""
-
+        """
+            Initialize the Student instance.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrs=None):
-        """Retrieve the dictionary representation of the student instance
-
-        Args:
-            - attrs: list of the attributes
-
-        Returns: A dict representation of an instance
+    def to_json(self, attr=None):
+        """
+            retrieve the dictionary representation of the Student.
+            Args:
+                attr (list): the attribute of names that are to be retrieved.
         """
 
-        my_dict = dict()
-        if type(attrs) is list and all(type(x) is str for x in attrs):
-            for x in attrs:
-                if x in self.__dict__:
-                    my_dict.upate({x: self.__dict__[x]})
-            return my_dict
-        return self.__dict__.copy()
+        if attr is not None:
+            res = {k: self.__dict__[k] for k in self.__dict__.keys() & attr}
+            return res
+        else:
+            return self.__dict__
