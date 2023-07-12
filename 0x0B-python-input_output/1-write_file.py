4fae49e8079f93e6f269429b619a81b425400b76
Gorileng <keneilwe963@gmail.com>
2023-07-11 17:41:20 -0700

    updated

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+"""
+    1-write_file: write_file()
+"""
 
 
 def write_file(filename="", text=""):
-    with open(filename, "w", encoding="UTF-8") as f:
-        return f.write(text)
+    """
+        write_file write the string to the text file.
+        Args:
+            filename (str): Name of the file.
+            text (str): A text to be written.
+        Returns: Number of the bytes that are written.
+    """
+    with open(filename, "w", encoding='utf-8') as a_file:
+        return a_file.write(text)
